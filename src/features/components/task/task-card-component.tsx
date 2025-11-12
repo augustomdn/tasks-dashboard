b5580e9f69834c55553af0ac774ff38f55540758
Augusto Meireles do Nascimento <augustomeirelesn@hotmail.com>
2025-11-12 17:57:55 -0300

    feat: finishing some ui adjusts

--- src/features/components/task/task-card-component.tsx
+++ src/features/components/task/task-card-component.tsx
@@ -22,25 +22,27 @@ export default function TaskCardComponent({
   return (
     <Card
       key={task.id}
-      className="p-4 flex flex-col justify-between h-64"
-    >
-      <div className="flex justify-between items-start">
-        <div>
-          <h3 className="text-lg font-semibold">{task.title}</h3>
-          <span className="text-xs text-gray-500 block">
-            Criado em: {formatDateTime(task.createdAt)}
-          </span>
+      className="p-4 flex flex-col justify-between min-h-56 sm:min-h-64 transition-all duration-150">
+      <div className="flex justify-between items-start gap-2">
+        <div className="min-w-0">
+          <h3 className="text-lg font-semibold truncate">{task.title}</h3>
+          <div className="flex gap-2">
+            <span className="text-sm text-gray-500 block">
+              Criado em: {formatDateTime(task.createdAt)}
+            </span>
+            <TaskPriorityBadgeComponent task={task.priority} />
+
+          </div>
         </div>
-        <div className="flex">
+        <div className="flex items-center gap-1 shrink-0">
           <AddTaskTagDialogComponent task={task} />
           <Button
             size="sm"
             variant="ghost"
             onClick={() => handleEditTask(task.id)}
           >
-            <Edit />
+            <Edit className="w-4 h-4" />
           </Button>
-
           <DeleteTaskConfirmDialogComponent
             handleDelete={handleDeleteTask}
             taskId={task.id}
@@ -49,33 +51,37 @@ export default function TaskCardComponent({
       </div>
 
       <div className="mt-2 flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent pr-1">
-        <p className="text-gray-700 italic text-sm leading-snug">
+        <p className="text-gray-700 italic text-sm leading-snug wrap-break-word">
           {task.description || "Sem descrição"}
         </p>
       </div>
 
-      <div className="mt-4 flex justify-between items-center">
-        <TaskPriorityBadgeComponent task={task.priority} />
-        <div className="flex gap-4">
-          <>
-            {task.tags?.slice(0, 2).map((tag, index) => (
+      <div className="mt-4 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+        <div className="w-full flex justify-between items-center gap-2">
+          <div className="flex gap-2 items-center">
+          {task.tags?.slice(0, 2).map((tag, index) => {
+            const displayName =
+            tag.name.length > 8 ? `${tag.name.slice(0, 8)}...` : tag.name;
+            
+            return (
               <span
-                key={index}
-                className={`${tag.color} flex items-center gap-1 px-2 py-1 rounded-md text-xs font-medium text-white`}
+              key={index}
+              className={`${tag.color} flex items-center gap-1 px-2 py-1 rounded-md text-xs font-medium text-white`}
               >
-                <Tag className="w-3 h-3"></Tag>
-                {tag.name}
+                <Tag className="w-3 h-3" />
+                {displayName}
               </span>
-            ))}
-            {/* {task.tags?.length > 2 && (
+            );
+          })}
+          {task.tags && task.tags.length > 2 && (
             <span className="text-xs text-gray-500 font-semibold">
               +{task.tags.length - 2}
             </span>
-          )} */}
-          </>
+          )}
+          </div>
+
           <TaskStatusIndicatorComponent task={task.status} />
         </div>
-
       </div>
     </Card>
   );
